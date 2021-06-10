47686ed44f0dab9b1f951cb5950a6a4bb6adf8d7
BeaverDeceiver <mr.bobrik2000@gmail.com>
2021-06-10 12:11:15 +0300

    feat/add beer list

---
+++ src/components/BeerList/BeerList.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectBeer } from '../../store/selectors/selectors';
+import { BeerItem } from '../BeerItem/BeerItem';
+
+export function BeerList() {
+  const beerItems = useSelector(selectBeer);
+
+  return (
+    <section className="beer-display">
+      <section className="beer-list">
+        {beerItems.map((item) => (
+          <article key={item.id}>
+            <BeerItem beer={item} />
+          </article>
+        ))}
+      </section>
+    </section>
+  );
+}
