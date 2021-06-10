cd27e6f82ce1fa2f2ed519a01798ca498d6c4c4a
BeaverDeceiver <mr.bobrik2000@gmail.com>
2021-06-10 10:03:00 +0300

    feat/update searchbar functionality, styles

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,19 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+import { useDispatch } from 'react-redux';
 import SearchIcon from '@material-ui/icons/Search';
+import { fetchBeer } from '../../store/actions/actions';
 
 export function SearchBar() {
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState('punk');
+  const dispatch = useDispatch();
+  const textInput = useRef(null);
+
+  function handleSearch(e) {
+    if (query !== '') dispatch(fetchBeer({ query }));
+    textInput.current.blur();
+  }
+
+  function handleKeyboard(e) {
+    setQuery(e.target.value);
+  }
 
   return (
     <>
       <form className="search" onSubmit={(e) => e.preventDefault()}>
         <input
+          ref={textInput}
           className="search__input"
           value={query}
-          onChange={(e) => setQuery(e.target.value)}
+          onChange={handleKeyboard}
           placeholder="Search beers..."
         />
-        <button className="search__button">
+        <button className="search__button" onClick={handleSearch}>
           <SearchIcon />
         </button>
       </form>
