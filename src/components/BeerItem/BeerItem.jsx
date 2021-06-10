3e977a8b68aded42feb5273d719f85ac22c9c193
BeaverDeceiver <mr.bobrik2000@gmail.com>
2021-06-10 12:12:18 +0300

    feat/add beer item

---
+++ src/components/BeerItem/BeerItem.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export function BeerItem(props) {
+  const { beer } = props;
+  return (
+    <>
+      <img
+        className="beer-item__image image_small"
+        src={beer.image_url}
+        alt="beer_image"
+      ></img>
+      <section className="beer-item__details">
+        <section>
+          <h1 className="beer-item__name">{beer.name}</h1>
+          <p className="beer-item__description">{beer.description}</p>
+        </section>
+        <section>
+          <button className="beer-item__button button button_transparent">
+            Open
+          </button>
+          <button className="beer-item__button button button_transparent">
+            Favorite
+          </button>
+        </section>
+      </section>
+    </>
+  );
+}
