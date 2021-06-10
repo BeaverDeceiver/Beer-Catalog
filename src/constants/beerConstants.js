e8309c48e2dd39a857c425b3c9449bc3e71a785a
BeaverDeceiver <mr.bobrik2000@gmail.com>
2021-06-10 15:39:04 +0300

    feat/add beer constants

---
+++ src/constants/beerConstants.js
@@ -0,0 +1,6 @@
+export const ALCOHOL_VOLUME_MIN = 2;
+export const ALCOHOL_VOLUME_MAX = 14;
+export const IBU_MIN = 0;
+export const IBU_MAX = 120;
+export const EBC_COLOR_MIN = 4;
+export const EBC_COLOR_MAX = 80;
