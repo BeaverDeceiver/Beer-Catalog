87d4bc1d186351e92ff408e198d29cef9b9c2d45
BeaverDeceiver <mr.bobrik2000@gmail.com>
2021-06-11 10:40:21 +0300

    feat/implement filtering (basic)

--- src/components/Home/BeerList/BeerList.jsx
+++ src/components/Home/BeerList/BeerList.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { selectBeer } from '../../../store/selectors/selectors';
+import {
+  selectBeer,
+  selectFilters,
+  selectFilterStatus,
+} from '../../../store/selectors/selectors';
 import { BeerItem } from '../BeerItem/BeerItem';
 
 export function BeerList() {
   const beerItems = useSelector(selectBeer);
+  const shouldFilter = useSelector(selectFilterStatus);
+  const filters = useSelector(selectFilters);
+
+  const shownItems = !shouldFilter
+    ? beerItems
+    : beerItems.filter((item) => {
+        return (
+          filters.abv >= item.abv &&
+          filters.ibu >= item.ibu &&
+          filters.ebc >= item.ebc
+        );
+      });
 
   return (
     <section className="beer-display">
       <section className="beer-list">
-        {beerItems.map((item) => (
+        {shownItems.map((item) => (
           <article key={item.id}>
             <BeerItem beer={item} />
           </article>
