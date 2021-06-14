b3d31fc2dd062f977b3483042cf37c1e3efa0615
BeaverDeceiver <mr.bobrik2000@gmail.com>
2021-06-14 18:52:31 +0300

    feat/add beer details page route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Home } from './components/Home/Home';
 import { Favorites } from './components//Favorites/Favorites';
 import './App.css';
+import { Details } from './components/Details/Details';
 function App() {
   return (
     <>
@@ -13,6 +14,7 @@ function App() {
           </header>
           <Route exact path="/" component={Home} />
           <Route path="/favorites" component={Favorites} />
+          <Route path="/beer/:beerId" component={Details} />
         </div>
       </Router>
     </>
