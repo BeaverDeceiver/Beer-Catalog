86fb2eb5a8c8edc8e36d6c2811b0e8254e491d36
BeaverDeceiver <mr.bobrik2000@gmail.com>
2021-06-16 13:49:47 +0300

    feat/add signup validation

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -4,6 +4,47 @@ import { Formik, Form, Field } from 'formik';
 import './SignUp.css';
 import { Link } from 'react-router-dom';
 
+const validateEmail = (value) => {
+  let errorMessage;
+
+  if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
+    errorMessage = 'Invalid email address';
+  }
+
+  return errorMessage;
+};
+
+const validateName = (value) => {
+  let errorMessage;
+
+  if (!/.{2,}/i.test(value)) {
+    errorMessage = 'Value too short';
+    return errorMessage;
+  }
+
+  if (/\d/i.test(value)) {
+    errorMessage = 'No numbers allowed';
+    return errorMessage;
+  }
+
+  if (/\s/i.test(value)) {
+    errorMessage = 'No whitespaces allowed';
+    return errorMessage;
+  }
+
+  return errorMessage;
+};
+
+const validatePassword = (value) => {
+  let errorMessage;
+
+  if (!/.{8,}/i.test(value)) {
+    errorMessage = 'Password too short (at least 8 symbols)';
+    return errorMessage;
+  }
+  return errorMessage;
+};
+
 export function SignUp() {
   return (
     <section className="sign-up">
@@ -13,74 +54,93 @@ export function SignUp() {
           firstName: '',
           lastName: '',
           email: '',
+          password: '',
         }}
         // onSubmit={async (values) => {}}
       >
-        <Form className="sign-up__form form">
-          <article className="form__input-area">
-            <label className="form__input-label" htmlFor="firstName">
-              First Name
-            </label>
-            <Field
-              className="form__input-field"
-              id="firstName"
-              name="firstName"
-              placeholder="Jane"
-            />
-          </article>
+        {({ errors, touched }) => (
+          <Form className="sign-up__form form">
+            <article className="form__input-area">
+              <label className="form__input-label" htmlFor="firstName">
+                First Name
+              </label>
+              <Field
+                className="form__input-field"
+                id="firstName"
+                name="firstName"
+                placeholder="Jane"
+                validate={validateName}
+              />
+              {errors.firstName && touched.firstName ? (
+                <div className="form__error-message">{errors.firstName}</div>
+              ) : null}
+            </article>
 
-          <article className="form__input-area">
-            <label className="form__input-label" htmlFor="lastName">
-              Last Name
-            </label>
-            <Field
-              className="form__input-field"
-              id="lastName"
-              name="lastName"
-              placeholder="Doe"
-            />
-          </article>
+            <article className="form__input-area">
+              <label className="form__input-label" htmlFor="lastName">
+                Last Name
+              </label>
+              <Field
+                className="form__input-field"
+                id="lastName"
+                name="lastName"
+                placeholder="Doe"
+                validate={validateName}
+              />
+              {errors.lastName && touched.lastName ? (
+                <div className="form__error-message">{errors.lastName}</div>
+              ) : null}
+            </article>
 
-          <article className="form__input-area">
-            <label className="form__input-label" htmlFor="email">
-              Email
-            </label>
-            <Field
-              className="form__input-field"
-              id="email"
-              name="email"
-              placeholder="mail@box.com"
-              type="email"
-            />
-          </article>
+            <article className="form__input-area">
+              <label className="form__input-label" htmlFor="email">
+                Email
+              </label>
+              <Field
+                className="form__input-field"
+                id="email"
+                name="email"
+                placeholder="mail@box.com"
+                type="email"
+                validate={validateEmail}
+              />
+              {errors.email && touched.email ? (
+                <div className="form__error-message">{errors.email}</div>
+              ) : null}
+            </article>
 
-          <article className="form__input-area">
-            <label className="form__input-label" htmlFor="password">
-              Password
-            </label>
-            <Field
-              className="form__input-field"
-              id="password"
-              name="password"
-              placeholder="Password"
-              type="password"
-            />
-          </article>
+            <article className="form__input-area">
+              <label className="form__input-label" htmlFor="password">
+                Password
+              </label>
+              <Field
+                className="form__input-field"
+                id="password"
+                name="password"
+                placeholder="Password"
+                type="password"
+                validate={validatePassword}
+              />
+              {errors.password && touched.password ? (
+                <div className="form__error-message">{errors.password}</div>
+              ) : null}
+            </article>
 
-          <article className="form__buttons-area">
-            <Link to="/signin">
-              <button className="form__button form__button_sign-in button">
-                Sign in instead
+            <article className="form__buttons-area">
+              <Link to="/signin">
+                <button className="form__button form__button_sign-in button">
+                  Sign in instead
+                </button>
+              </Link>
+              <button
+                className="form__button form__button_sign-up button"
+                type="submit"
+              >
+                Sign Up
               </button>
-            </Link>
-            <button
-              className="form__button form__button_sign-up button"
-              type="submit"
-            >
-              Sign Up
-            </button>
-          </article>
-        </Form>
+            </article>
+          </Form>
+        )}
       </Formik>
     </section>
   );
