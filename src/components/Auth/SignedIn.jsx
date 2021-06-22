9ae92e50921decffa89097e1d4d159dcc10de740
BeaverDeceiver <mr.bobrik2000@gmail.com>
2021-06-22 15:08:26 +0300

    feat/add signed in placeholder

---
+++ src/components/Auth/SignedIn.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { hardLogOut } from './Session';
+import { Link } from 'react-router-dom';
+import './SignedIn.css';
+
+export function SignedIn() {
+  function handleSignOut() {
+    hardLogOut();
+  }
+
+  return (
+    <section className="signed-in">
+      <h1 className="form__header">You are signed in</h1>
+      <section className="form__buttons-area">
+        <Link to="/auth/signin">
+          <button
+            className="form__button button signed-in__button"
+            onClick={handleSignOut}
+          >
+            Sign Out
+          </button>
+        </Link>
+        <Link to="/">
+          <button className="form__button button signed-in__button">
+            Homepage
+          </button>
+        </Link>
+      </section>
+    </section>
+  );
+}
