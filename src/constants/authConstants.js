b8d1decf92a552c91b6bf09b21c7f6b8dac2b347
BeaverDeceiver <mr.bobrik2000@gmail.com>
2021-06-23 16:10:40 +0300

    feat/add favorites filter status constants

---
+++ src/constants/authConstants.js
@@ -0,0 +1,2 @@
+export const AUTH_STATUS_LOGGED_IN = true;
+export const AUTH_STATUS_LOGGED_OUT = false;
--- src/constants/stateConstants.js
+++ src/constants/stateConstants.js
@@ -2,3 +2,6 @@ export const STATE_STATUS_IDLE = 'idle';
 export const STATE_STATUS_BUSY = 'busy';
 
 export const PER_PAGE = 9;
+
+export const FAVORITES_STATUS_UNSET = false;
+export const FAVORITES_STATUS_SET = true;
