c3625027c6b567bc516d1bd93548ffb79ea9459c
BeaverDeceiver <mr.bobrik2000@gmail.com>
2021-06-23 14:25:08 +0300

    feat/add authorisation header to fetch requests

--- src/apis/Fetch.js
+++ src/apis/Fetch.js
@@ -1,18 +1,25 @@
+import { getAccessToken } from './Session';
 import { getSingleBeerURL } from './URL';
 
 export async function fetchAPI(url) {
-  const resolve = await fetch(url);
+  const resolve = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${getAccessToken()}`,
+      'Content-Type': 'application/json',
+    },
+  });
   return await resolve.json();
 }
 
 export async function fetchSingleBeer(id) {
   const url = getSingleBeerURL(id);
-  const data = await fetchAPI(url);
-  return data[0];
-}
-
-export async function fetchSingleBeerBackend(id) {
-  const url = getSingleBeerURL(id);
-  const data = await (await fetch(url)).json();
+  const data = await (
+    await fetch(url, {
+      headers: {
+        Authorization: `Bearer ${getAccessToken()}`,
+        'Content-Type': 'application/json',
+      },
+    })
+  ).json();
   return data[0];
 }
