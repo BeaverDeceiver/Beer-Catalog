b4f024dabf435e9ca01733ff5006229e8c7d7e1d
BeaverDeceiver <mr.bobrik2000@gmail.com>
2021-06-24 16:30:30 +0300

    feat/moved header to list, fix favorites loading

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -4,7 +4,6 @@ import { FavoritesList } from './FavoritesList/FavoritesList';
 export function Favorites() {
   return (
     <>
-      <h1 className="favorites__header">Your favorite beers</h1>
       <FavoritesList />
     </>
   );
