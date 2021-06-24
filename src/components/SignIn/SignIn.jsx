2123653af5ba9c8bad661736ea00ca8cf476c293
BeaverDeceiver <mr.bobrik2000@gmail.com>
2021-06-24 15:46:43 +0300

    feat/add load placeholder

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Formik, Form, Field } from 'formik';
-
-import './SignIn.css';
 import { Link, Redirect } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { LinearProgress } from '@material-ui/core';
+
+import { SignedIn } from '../Auth/SignedIn';
 import { sendSignInRequest } from '../../apis/Auth';
 import { getAccessToken, setTokens } from '../../apis/Session';
-import { SignedIn } from '../Auth/SignedIn';
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
 import { setAuthStatus } from '../../store/actions/actions';
 import { AUTH_STATUS_LOGGED_IN } from '../../constants/authConstants';
+
+import './SignIn.css';
 
 const validateEmail = (value) => {
   let errorMessage;
@@ -24,6 +25,7 @@ const validateEmail = (value) => {
 export function SignIn() {
   const [redirect, setRedirect] = useState(null);
   const [invalidData, setInvalidData] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
 
   const dispatch = useDispatch();
 
@@ -41,76 +43,84 @@ export function SignIn() {
   }
 
   return (
-    <section className="sign-up">
-      <h1 className="sign-up__header form__header">Sign In</h1>
-      <Formik
-        initialValues={{
-          email: '',
-          password: '',
-        }}
-        onSubmit={async (values) => {
-          try {
-            const response = await sendSignInRequest(values);
-            setTokens(response);
-            dispatch(setAuthStatus({ status: AUTH_STATUS_LOGGED_IN }));
+    <>
+      <section className="load-placeholder">
+        {submitted ? <LinearProgress /> : null}
+      </section>
+      <section className="sign-up">
+        <h1 className="sign-up__header form__header">Sign In</h1>
+        <Formik
+          initialValues={{
+            email: '',
+            password: '',
+          }}
+          onSubmit={async (values) => {
+            setSubmitted(true);
+            try {
+              const response = await sendSignInRequest(values);
+              setTokens(response);
+              dispatch(setAuthStatus({ status: AUTH_STATUS_LOGGED_IN }));
+              setSubmitted(false);
 
-            setRedirect(true);
-          } catch (e) {
-            handleInvalidData();
-          }
-        }}
-      >
-        {({ errors, touched }) => (
-          <Form className="sign-in__form">
-            <article className="form__input-area">
-              <label className="form__input-label" htmlFor="email">
-                Email
-              </label>
-              <Field
-                className="form__input-field"
-                id="email"
-                name="email"
-                placeholder="mail@box.com"
-                type="email"
-                validate={validateEmail}
-              />
-              {errors.email && touched.email ? (
-                <div className="form__error-message">{errors.email}</div>
-              ) : null}
-            </article>
+              setRedirect(true);
+            } catch (e) {
+              setSubmitted(false);
+              handleInvalidData();
+            }
+          }}
+        >
+          {({ errors, touched }) => (
+            <Form className="sign-in__form">
+              <article className="form__input-area">
+                <label className="form__input-label" htmlFor="email">
+                  Email
+                </label>
+                <Field
+                  className="form__input-field"
+                  id="email"
+                  name="email"
+                  placeholder="mail@box.com"
+                  type="email"
+                  validate={validateEmail}
+                />
+                {errors.email && touched.email ? (
+                  <div className="form__error-message">{errors.email}</div>
+                ) : null}
+              </article>
 
-            <article className="form__input-area">
-              <label className="form__input-label" htmlFor="password">
-                Password
-              </label>
-              <Field
-                className="form__input-field"
-                id="password"
-                name="password"
-                placeholder="Password"
-                type="password"
-              />
-            </article>
+              <article className="form__input-area">
+                <label className="form__input-label" htmlFor="password">
+                  Password
+                </label>
+                <Field
+                  className="form__input-field"
+                  id="password"
+                  name="password"
+                  placeholder="Password"
+                  type="password"
+                />
+              </article>
 
-            <article className="form__buttons-area">
-              <Link to="/auth/signup">
-                <button className="form__button form__button_sign-in button">
-                  Sign up instead
+              <article className="form__buttons-area">
+                <Link to="/auth/signup">
+                  <button className="form__button form__button_sign-in button">
+                    Sign up instead
+                  </button>
+                </Link>
+                <button
+                  className="form__button form__button_sign-up button"
+                  type="submit"
+                >
+                  Sign In
                 </button>
-              </Link>
-              <button
-                className="form__button form__button_sign-up button"
-                type="submit"
-              >
-                Sign In
-              </button>
-            </article>
-            {invalidData ? (
-              <div className="auth__error">Invalid Email and/or Password</div>
-            ) : null}
-          </Form>
-        )}
-      </Formik>
-    </section>
+              </article>
+              {invalidData ? (
+                <div className="auth__error">Invalid Email and/or Password</div>
+              ) : null}
+            </Form>
+          )}
+        </Formik>
+      </section>
+    </>
   );
 }
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Formik, Form, Field } from 'formik';
-
-import './SignUp.css';
+import { LinearProgress } from '@material-ui/core';
 import { Link, Redirect } from 'react-router-dom';
+
 import { sendSignUpRequest } from '../../apis/Auth';
 import { getAccessToken } from '../../apis/Session';
 import { SignedIn } from '../Auth/SignedIn';
-import { useState } from 'react';
+
+import './SignUp.css';
 
 const validateEmail = (value) => {
   let errorMessage;
@@ -53,6 +54,7 @@ export function SignUp() {
   const [registered, setRegistered] = useState(false);
   const [invalidData, setInvalidData] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
+  const [submitted, setSubmitted] = useState(false);
 
   const handleInvalidData = (e) => {
     setInvalidData(true);
@@ -69,111 +71,119 @@ export function SignUp() {
   }
 
   return (
-    <section className="sign-up">
-      <h1 className="sign-up__header form__header">Sign Up</h1>
-      <Formik
-        initialValues={{
-          firstName: '',
-          lastName: '',
-          email: '',
-          password: '',
-        }}
-        onSubmit={async (values) => {
-          try {
-            const response = await sendSignUpRequest(values);
-            if (response) {
-              setRegistered(true);
+    <>
+      <section className="load-placeholder">
+        {submitted ? <LinearProgress /> : null}
+      </section>
+      <section className="sign-up">
+        <h1 className="sign-up__header form__header">Sign Up</h1>
+        <Formik
+          initialValues={{
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: '',
+          }}
+          onSubmit={async (values) => {
+            setSubmitted(true);
+            try {
+              const response = await sendSignUpRequest(values);
+              if (response) {
+                setSubmitted(false);
+                setRegistered(true);
+              }
+            } catch (e) {
+              handleInvalidData(e);
+              setSubmitted(false);
             }
-          } catch (e) {
-            handleInvalidData(e);
-          }
-        }}
-      >
-        {({ errors, touched }) => (
-          <Form className="sign-up__form form">
-            <article className="form__input-area">
-              <label className="form__input-label" htmlFor="firstName">
-                First Name
-              </label>
-              <Field
-                className="form__input-field"
-                id="firstName"
-                name="firstName"
-                placeholder="Jane"
-                validate={validateName}
-              />
-              {errors.firstName && touched.firstName ? (
-                <div className="form__error-message">{errors.firstName}</div>
-              ) : null}
-            </article>
+          }}
+        >
+          {({ errors, touched }) => (
+            <Form className="sign-up__form form">
+              <article className="form__input-area">
+                <label className="form__input-label" htmlFor="firstName">
+                  First Name
+                </label>
+                <Field
+                  className="form__input-field"
+                  id="firstName"
+                  name="firstName"
+                  placeholder="Jane"
+                  validate={validateName}
+                />
+                {errors.firstName && touched.firstName ? (
+                  <div className="form__error-message">{errors.firstName}</div>
+                ) : null}
+              </article>
 
-            <article className="form__input-area">
-              <label className="form__input-label" htmlFor="lastName">
-                Last Name
-              </label>
-              <Field
-                className="form__input-field"
-                id="lastName"
-                name="lastName"
-                placeholder="Doe"
-                validate={validateName}
-              />
-              {errors.lastName && touched.lastName ? (
-                <div className="form__error-message">{errors.lastName}</div>
-              ) : null}
-            </article>
+              <article className="form__input-area">
+                <label className="form__input-label" htmlFor="lastName">
+                  Last Name
+                </label>
+                <Field
+                  className="form__input-field"
+                  id="lastName"
+                  name="lastName"
+                  placeholder="Doe"
+                  validate={validateName}
+                />
+                {errors.lastName && touched.lastName ? (
+                  <div className="form__error-message">{errors.lastName}</div>
+                ) : null}
+              </article>
 
-            <article className="form__input-area">
-              <label className="form__input-label" htmlFor="email">
-                Email
-              </label>
-              <Field
-                className="form__input-field"
-                id="email"
-                name="email"
-                placeholder="mail@box.com"
-                type="email"
-                validate={validateEmail}
-              />
-              {errors.email && touched.email ? (
-                <div className="form__error-message">{errors.email}</div>
-              ) : null}
-            </article>
+              <article className="form__input-area">
+                <label className="form__input-label" htmlFor="email">
+                  Email
+                </label>
+                <Field
+                  className="form__input-field"
+                  id="email"
+                  name="email"
+                  placeholder="mail@box.com"
+                  type="email"
+                  validate={validateEmail}
+                />
+                {errors.email && touched.email ? (
+                  <div className="form__error-message">{errors.email}</div>
+                ) : null}
+              </article>
 
-            <article className="form__input-area">
-              <label className="form__input-label" htmlFor="password">
-                Password
-              </label>
-              <Field
-                className="form__input-field"
-                id="password"
-                name="password"
-                placeholder="Password"
-                type="password"
-                validate={validatePassword}
-              />
-              {errors.password && touched.password ? (
-                <div className="form__error-message">{errors.password}</div>
-              ) : null}
-            </article>
+              <article className="form__input-area">
+                <label className="form__input-label" htmlFor="password">
+                  Password
+                </label>
+                <Field
+                  className="form__input-field"
+                  id="password"
+                  name="password"
+                  placeholder="Password"
+                  type="password"
+                  validate={validatePassword}
+                />
+                {errors.password && touched.password ? (
+                  <div className="form__error-message">{errors.password}</div>
+                ) : null}
+              </article>
 
-            <article className="form__buttons-area">
-              <Link to="/auth/signin">
-                <button className="form__button form__button_sign-in button">
-                  Sign in instead
+              <article className="form__buttons-area">
+                <Link to="/auth/signin">
+                  <button className="form__button form__button_sign-in button">
+                    Sign in instead
+                  </button>
+                </Link>
+                <button
+                  className="form__button form__button_sign-up button"
+                  type="submit"
+                >
+                  Sign Up
                 </button>
-              </Link>
-              <button
-                className="form__button form__button_sign-up button"
-                type="submit"
-              >
-                Sign Up
-              </button>
-            </article>
-          </Form>
-        )}
-      </Formik>
-      {invalidData ? <div className="auth__error">{errorMessage}</div> : null}
-    </section>
+              </article>
+            </Form>
+          )}
+        </Formik>
+        {invalidData ? <div className="auth__error">{errorMessage}</div> : null}
+      </section>
+    </>
   );
 }
